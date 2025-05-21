9471b4926aa61a7751155422612b5d189a7d3946
kamorenjoroge <franciskamorenjoroge@gmail.com>
2025-05-21 16:59:20 +0300

    first commit

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -3,7 +3,7 @@ const Page = () => {
   return (
     <div className='flex flex-col items-center justify-center min-h-screen text-7xl bg-back p-24'>
      <div className=' bg-primary text-white p-10 rounded-lg'>
-      <Link href='/test'>Click Me </Link>
+      <Link href='/'>Click Me </Link>
       </div>
       
     </div>
